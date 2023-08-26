8694db334772de42e1788cac01d872b1670ff118
Sasza44 <110614574+Sasza44@users.noreply.github.com>
2023-08-26 21:26:56 +0300

    Add files via upload

--- Homework6/src/synchronous/SearchFile.java
+++ Homework6/src/synchronous/SearchFile.java
@@ -1,6 +1,8 @@
 package synchronous;
 
 import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SearchFile implements Runnable{
 	private String fileName;
@@ -13,14 +15,16 @@ public class SearchFile implements Runnable{
 	}
 	
 	private void searchFile() {
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
 		File[] files = search.listFiles();
 		for(File f: files) {
 			if(f.isFile() && f.getName().equals(fileName)) {
 				System.out.println(f.getAbsolutePath());
 			}
 			else if(f.isDirectory()) {
-				Thread thread = new Thread(new SearchFile(fileName, f));
-				thread.start();
+//				Thread thread = new Thread(new SearchFile(fileName, f));
+//				thread.start();
+				executorService.execute(new SearchFile(fileName, f));
 			}
 		}
 	}
